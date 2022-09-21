465d319a5fe06c45e787cb847edfda7c1776678e
Nikita Hasan Kafes <kafes-nik@mail.ru>
2022-09-21 21:56:47 +0300

    Second week

--- src/oop1/Main.java
+++ src/oop1/Main.java
@@ -1,8 +1,18 @@
-
 package oop1;
 
 public class Main {
     public static void main(String[] args) {
+        String mesaj = "Vade orani";
 
+        Product product1 = new Product();
+                //set value
+        product1.name = "Delonghi Kahve Makinesi";
+        product1.unitPrice = 7500;
+        product1.discount = 7;
+        product1.unitsInStock = 3;
+        product1.imageUrl = "coff.jpg";
+
+                //get value
+        System.out.println(product1.name);
     }
 }
