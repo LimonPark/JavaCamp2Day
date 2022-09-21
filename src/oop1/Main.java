dcfe529c0c910f6ef28907dced4d8ee44a571c29
Nikita Hasan Kafes <kafes-nik@mail.ru>
2022-09-21 23:26:49 +0300

    Second week

--- src/oop1/Main.java
+++ src/oop1/Main.java
@@ -6,13 +6,34 @@ public class Main {
 
         Product product1 = new Product();
                 //set value
-        product1.name = "Delonghi Kahve Makinesi";
-        product1.unitPrice = 7500;
-        product1.discount = 7;
-        product1.unitsInStock = 3;
-        product1.imageUrl = "coff.jpg";
-
+        product1.setName("Delonghi Kahve Makinesi");
+        product1.setUnitPrice(7500);
+        product1.setDiscount(7);
+        product1.setUnitsInStock(3);
+        product1.setImageUrl("coff.jpg");
                 //get value
-        System.out.println(product1.name);
+        //System.out.println(product1.name);
+
+        Product product2 = new Product();
+        product2.setName("Smeg Kahve Makinesi");
+        product2.setUnitPrice(6500);
+        product2.setDiscount(8);
+        product2.setUnitsInStock(2);
+        product2.setImageUrl("coff2.jpg");
+
+        Product product3 = new Product();
+        product3.setName("Kitchen Aid Kahve Makinesi");
+        product3.setUnitPrice(4500);
+        product3.setDiscount(9);
+        product3.setUnitsInStock(4);
+        product3.setImageUrl("coff3.jpg");
+
+        Product[] products = {product1, product2, product3};
+
+        System.out.println("<ul>");
+        for (Product product : products) {
+            System.out.println("<li>" + product.getName() + "</li>");
+        }
+        System.out.println("</ul>");
     }
 }
--- src/oop1/Product.java
+++ src/oop1/Product.java
@@ -1,9 +1,41 @@
 package oop1;
 
 public class Product {
-    String name;
-    double unitPrice;
-    double discount;
-    String imageUrl;
-    int unitsInStock;
+
+    private String name;
+    private double unitPrice;
+    private double discount;
+    private String imageUrl;
+    private int unitsInStock;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public double getUnitPrice(){
+        return getUnitPrice();
+    }
+    public void setUnitPrice(double setUnitPrice){
+        this.unitPrice = setUnitPrice;
+    }
+    public double getDiscount(){
+        return discount;
+    }
+    public void setDiscount(double discount){
+        this.discount = discount;
+    }
+    public String getImageUrl(){
+        return imageUrl;
+    }
+    public void setImageUrl(String imageUrl){
+        this.imageUrl = imageUrl;
+    }
+    public int getUnitsInStock(){
+        return unitsInStock;
+    }
+    public void setUnitsInStock(int unitsInStock){
+        this.unitsInStock = unitsInStock;
+    }
 }
